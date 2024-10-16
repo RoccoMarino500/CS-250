6272ca1365bd8c35d9c1ac25c999eb366b59d170
Rocco <145149422+RoccoMarino500@users.noreply.github.com>
2024-10-15 21:33:02 -0400

    Program Two: OO Concepts
    The purpose to provide a first experience with defining and using classes.
    
    Scenario: You have been tasked with writing classes that can be used by the athletics' department at a local university.  The classes you are implement must be exactly as specified by the following UML class diagram.
    
    Use to test your classes
    
    int main()
    {
        Athlete sport(72, 140, 'M');
        cout << "Athlete: " << sport.getHeight() << " " 
             << sport.getWeight() << " " 
             << sport.getGender() << endl;
    
        Tennis martina = Tennis();
        martina.setServeSpeed(100);
        martina.setGender('F');
        Tennis bjorn(150,false);
        bjorn.setGender('M');
        if  (martina.getGender() == 'F')
           cout << "Martina serve speed: " << martina.getServerSpeed() << endl;
    
        VolleyBall kerri = VolleyBall();
        kerri.setGender('F');
        kerri.setPosition("Opposite Hitter");
        VolleyBall giba("Outside Hitter",23.2);
        giba.setGender('M');
        if  (kerri.getGender() == 'F')
           cout << "Kerri is an " << kerri.getPosition() << endl;
    
        return 0;
    }

---
+++ main.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+
+    Code to determine Althletes department at a local university
+    
+    Author: Rocco Marino
+    Due Date: 10/15/2024
+
+*******************************************************************************/
+#include <iostream>
+#include "Athlete.h"
+#include "Tennis.h"
+#include "Volleyball.h"
+
+using namespace std;
+
+int main() {
+    Athlete sport(72, 140, 'M');
+    cout << "Athlete: " << sport.getHeight() << " " 
+         << sport.getWeight() << " " 
+         << sport.getGender() << endl;
+
+    Tennis martina = Tennis();
+    martina.setServeSpeed(100);
+    martina.setGender('F');
+    Tennis bjorn(150, false);
+    bjorn.setGender('M');
+    if (martina.getGender() == 'F')
+       cout << "Martina serve speed: " << martina.getServeSpeed() << endl;
+
+    VolleyBall kerri = VolleyBall();
+    kerri.setGender('F');
+    kerri.setPosition("Opposite Hitter");
+    VolleyBall giba("Outside Hitter", 23.2);
+    giba.setGender('M');
+    if (kerri.getGender() == 'F')
+       cout << "Kerri is an " << kerri.getPosition() << endl;
+
+    return 0;
+}
